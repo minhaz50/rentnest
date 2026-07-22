07a5cc0cb72402a0cd8321713a371006b6b9ff5b
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-07-22 15:36:26 +0600

    write code for review

---
+++ src/modules/review/review.service.ts
@@ -0,0 +1,10 @@
+const createReview = async (
+  tenantId: string,
+  rentalRequestId: string,
+  rating: number,
+  comment?: string,
+) => {};
+
+export const reviewService = {
+  createReview,
+};
