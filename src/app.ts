2e3858f050871e5e850b9b5a48a0134686c475b7
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-07-23 22:16:49 +0600

    user  code updated

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
-import express from "express";
-import type { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import authRoutes from "./modules/auth/auth.routes";
+import userRoutes from "./modules/user/user.routes";
+import categoryRoutes from "./modules/category/category.routes";
+import {
+  publicPropertyRouter,
+  landlordPropertyRouter,
+  adminPropertyRouter,
+} from "./modules/property/property.routes";
+import {
+  rentalRouter,
+  landlordRentalRouter,
+  adminRentalRouter,
+} from "./modules/rental/rental.routes";
+import paymentRoutes from "./modules/payment/payment.routes";
+import { stripeWebhook } from "./modules/payment/payment.controller";
+import reviewRoutes from "./modules/review/review.routes";
+
+import { notFound, errorHandler } from "./middleware/errorHandler";
 
 const app: Application = express();
 
 app.use(cors());
+app.use(morgan("dev"));
+
+app.post(
+  "/api/payments/stripe/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhook,
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,5 +40,22 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/properties", publicPropertyRouter);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/rentals", rentalRouter);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/reviews", reviewRoutes);
+
+// landlord
+app.use("/api/landlord/properties", landlordPropertyRouter);
+app.use("/api/landlord/requests", landlordRentalRouter);
+
+// admin-only routes
+app.use("/api/admin/users", userRoutes);
+app.use("/api/admin/properties", adminPropertyRouter);
+app.use("/api/admin/rentals", adminRentalRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app";
 
-const port = Number(process.env.PORT) || 5000;
+export default app;
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
